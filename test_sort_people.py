8a717cbebe6dddec3b3226dea7641d8428142d13
arielramirez <arielmccree@gmail.com>
2019-09-04 11:28:20 -0500

    fixing tests

--- test_sort_people.py
+++ test_sort_people.py
@@ -1,9 +1,9 @@
-
 import unittest
 from sort_people import sort_people
 import json
 
 TEST_FILE = 'test_people.json'
+
 def get_test_people():
     with open(TEST_FILE) as json_file:
         orig_people = json.load(json_file)
@@ -12,15 +12,16 @@ def get_test_people():
 class TestPeopleSort(unittest.TestCase):
 
     def test_sort_by_name(self):
-            names = [person["name"] for person in get_test_people()]
-            self.assertEquals(names, sorted(names))
+        names = [person["name"] for person in sort_people(get_test_people())]
+        self.assertEqual(names, sorted(names))
 
     def test_sort_by_age_in_names(self):
-            orig_people = get_test_people()
-            last_person = orig_people[0]
-            for curr_person in orig_people[1:]:
-                if last_person["name"] == curr_person["name"]:
-                    self.assertGreaterEqual(last_person.age, curr_person.age)
+        sorted_people = sort_people(get_test_people())
+        prev_person = sorted_people[0]
+        for curr_person in sorted_people[1:]:
+            if prev_person["name"] == curr_person["name"]:
+                self.assertGreaterEqual(prev_person['age'], curr_person['age'])
+            prev_person = curr_person
 
     def test_sort_order_when_name_and_age_match(self):
         def get_index(lst, key, value):
@@ -32,17 +33,21 @@ class TestPeopleSort(unittest.TestCase):
 
         orig_people = get_test_people()
         sorted_people = sort_people(orig_people)
-        last_person = sorted_people[0]
+        prev_person = sorted_people[0]
         for curr_person in sorted_people[1:]:
-            if last_person.name == curr_person.name & last_person.age == curr_person.age:
+            if prev_person['name'] == curr_person['name'] and prev_person['age'] == curr_person['age']:
                 # ensures that the order in the sorted list matches the order in the original list
-                self.assertLess(get_index(orig_people, "ssn", last_person.ssn), get_index(orig_people, "ssn", curr_person.ssn))
+                self.assertLess(get_index(orig_people, "ssn", prev_person['ssn']), get_index(orig_people, "ssn", curr_person['ssn']))
 
     def test_data_integrity(self):
         orig_people = get_test_people()
         sorted_people = sort_people(orig_people)
-        # ensure all fields remain and are the same
-        # for person in orig_people:
+        for orig_person in orig_people:
+            sorted_person_list = [person for person in sorted_people if person['ssn'] == orig_person['ssn'] ]
+            self.assertTrue(len(sorted_person_list) == 1)
+            sorted_person = sorted_person_list[0]
+            for key, value in orig_person.items(): 
+                self.assertTrue(sorted_person[key] == value)
             
                     
             
